a9b6a7d5ae01d6081dd2cfdbd8b0906b62b56e4b
stephamundson <stephamundson@gmail.com>
2022-12-05 19:09:39 -0600

    Minor refactoring

--- starter/lib/utils.dart
+++ starter/lib/utils.dart
@@ -1,34 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'activity.dart';
 
-List<Activity> getActivities() {
-  // method will be changed to interact with the database to only
-  // pull activities whose date matches the date in the parameter
+/// gets all activities from the database
+Future<QuerySnapshot> getAllActivities() {
   final allActivities = FirebaseFirestore.instance.collection('activities');
-  List<Activity> validActivities = [];
-  var query = allActivities;
-  query.get().then((querySnapshot) {
-    for (var doc in querySnapshot.docs) {
-      // setState(() {
-      //setstate????
-      Activity currentActivity = Activity(
-          title: doc['title'],
-          description: doc['description'],
-          date: DateTime.parse(doc['date']));
-      validActivities.add(currentActivity);
-      // ignore: avoid_print
-      print(currentActivity.toString());
-      // });
-    }
-  });
-
-  // List<Activity> activities = HomeScreen().list;
-  // for (var element in activities) {
-  //   if (isSameDay(element.date, day)) {
-  //     validActivities.add(element);
-  //   }
-  // }
-  return validActivities;
+  return allActivities.get();
 }
 
 /// Gets the activities for the given [day]
